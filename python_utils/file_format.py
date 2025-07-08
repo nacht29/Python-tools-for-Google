7e88d747e666ed927d3f8b9f4dc7b5a73cbfd489
nacht29 <nacht29.study@gmail.com>
2025-07-09 01:26:19 +0800

    refactor: reupload python_utils

--- python_utils/file_format.py
+++
@@ -1,6 +0,0 @@
-content_data = {
-	'.csv': {'content_type': 'text/csv', 'type_name': 'CSV'},
-	'.txt': {'content_type': 'text/plain', 'type_name': 'Text'},
-	'.xlsx': {'content_type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'type_name': 'Excel'},
-	'.log': {'content_type': 'text/plain', 'type_name': 'Log'}
-}
